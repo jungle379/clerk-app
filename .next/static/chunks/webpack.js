1d60bf1b8c27cf3266c2670845348cfa217f732f
jungle379 <jungle379@gmail.com>
2021-07-20 21:39:38 +0900

    🐛

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "88a48729776c821aa3aa"; }
+/******/ 		__webpack_require__.h = function() { return "16f402baf34e6592df63"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.jsx
+++ pages/index.jsx
@@ -91,6 +91,17 @@ const Main = () => (
         </a>
       </Link>
     </div>
+      <div>
+        <h1>
+          <Link href="../result">今週の予想</Link>
+        </h1>
+        <h2>
+          <Link href="../memo">好走記録</Link>
+        </h2>
+	      <h3>
+	        <Link href="../trend">傾向</Link>
+	      </h3>
+      </div>
   </main>
 );
 
