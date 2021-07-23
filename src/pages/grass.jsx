976bc1d771258967ed0bee05e8dd7fcd1df64770
jungle379 <jungle379@gmail.com>
2021-07-23 17:30:11 +0900

    🔥

--- src/pages/grass.jsx
+++ src/pages/grass.jsx
@@ -1,48 +1,52 @@
 import Link from "next/link";
+import Head from "next/head"
 
 const Grass = () => {
   return <>
+  <Head>
+    <title>芝の勝利馬一覧</title>
+  </Head>
     <div>芝の勝利馬</div>
-    <h1>
+    <div>
     <li>ビアンフェ</li>
     <ul>函館スプリントS・1200m・G3・札幌・藤岡佑</ul>
     <ul>逃げ・瞬発力があるタイプではなくじわっと持続力がある逃げ方をする。去勢後の良さが出てきた。</ul>
     <Link href="https://db.sp.netkeiba.com/horse/2017101832/">・JRAページ</Link>
-    </h1>
-    <h2>
+    </div>
+    <div>
     <li>サダル</li>
     <ul>エプソムC・1800m・G3・東京・石橋</ul>
     <ul>差し・長期休養が功を奏し、外枠も生かしての外差しで決着。+12kgで504kgも問題なかった。</ul>
     <Link href="https://db.sp.netkeiba.com/horse/2016104772/">・JRAページ</Link>
-    </h2>
-    <h3>
+    </div>
+    <div>
     <li>カレンシュトラウス</li>
     <ul>桑名S・1800m・OP・中京・池添</ul>
     <ul>差し・OPを2勝後、迎えた本戦。外からの豪脚によって一気に差し切る。ルーラーシップ産駒に珍しく早い上がりにも対応可</ul>
     <Link href="https://db.sp.netkeiba.com/horse/2017102897/">・JRAページ</Link>
-    </h3>
-    <h4>
+    </div>
+    <div>
       <li>クロノジェネシス</li>
       <ul>宝塚記念・2200m・G1・阪神・ルメール</ul>
       <ul>前に３頭を置き、終始先団の位置。直線に入った時点でエンジンをかけて逃げ２頭を差し切り２馬身半差。凱旋門も狙える動き</ul>
       <Link href="https://db.sp.netkeiba.com/horse/2016104750/">・JRAページ</Link>
-    </h4>
-    <h5>
+    </div>
+    <div>
       <li>エスコーラ</li>
       <ul>3歳未勝利・1800m・小倉・川田</ul>
       <ul>1800mの日本Rを更新。新馬戦では未熟な所があり、不良馬場に梃子摺ったが今回は平均35秒前半の上がりタイムの中、１頭だけ34.2をマーク。経験を積んでもらう必要があるが、良馬場メインで良いかもしれない。</ul>
       <Link href="https://db.sp.netkeiba.com/horse/2018105043/">・JRAページ</Link>
-    </h5>
-    <h5>
+    </div>
+    <div>
       <li>ファストフォース</li>
       <ul>CBC賞・1200m・G3・小倉・鮫島(克)</ul>
       <ul>芝1200mのレコードを更新。スタートと共に騎手に促されハイペースのまま1分6秒でのゴールと逃げ切りの形となった。開幕の内枠は狙った方が良い</ul>
-    </h5>
-    <h5>
+    </div>
+    <div>
       <li>ヴァイスメテオール</li>
       <ul>ラジオNIKKEI賞・1800m・G3・福島・丸山</ul>
       <ul>良馬場より多少渋った方が伸びるタイプ。上がりタイムを求められるコンディションよりじわっと長い末脚を使う場合にフィットする。ダービートライアルにルメールも騎乗。</ul>
-      </h5>
+      </div>
 
     </>;
 };
--- src/pages/sand.jsx
+++ src/pages/sand.jsx
@@ -1,22 +1,25 @@
 import Link from "next/link";
-import Layout from "../src/components/layout";
+import Head from "next/head"
 
 const Sand = () =>{
-  return <Layout>
+  return <>
+  <Head>
+    <title>砂の勝利馬一覧</title>
+  </Head>
     <div>ダートの勝利馬</div>
-    <h1>
+    <div>
       <li>ハヤヤッコ</li>
       <ul>スレイプニルS・2100m・OP・東京・田辺</ul>
       <ul>大箱の競馬場が得意なのであろうか、４コーナーの終わりまで一番後ろで待機し、後方一期の末脚で差し切った。左の非根幹距離を狙う</ul>
     <Link href="https://db.sp.netkeiba.com/horse/2016104624/">JRAページ</Link>
-    </h1>
-    <h2>
+    </div>
+    <div>
       <li>テーオーケインズ</li>
       <ul>帝王賞・2000m・Jpn1・大井・松山</ul>
       <ul>道中を4番手ぐらいで進め、前がきつい展開で４コーナーを迎えるに当たって、１頭分空いた内を掬う形で進出。直線での勢いからはG1を勝利する実力はあった。</ul>
     <Link href="https://db.sp.netkeiba.com/horse/2017101010/">JRAページ</Link>
-    </h2>
-    </Layout>;
+    </div>
+    </>;
 };
 
 export default Sand;
