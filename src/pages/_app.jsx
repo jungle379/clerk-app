c5ae11164c97e1a15a346c361b19daacbeab69c5
jungle379 <jungle379@gmail.com>
2021-07-24 21:38:14 +0900

    ⚡️

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import "tailwindcss/tailwind.css";
 
 import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from '@clerk/clerk-react'
 import { useRouter } from 'next/router'
