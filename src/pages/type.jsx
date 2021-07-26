a087a8a0ab352f6b90461f2aae007b9641786ed1
jungle379 <jungle379@gmail.com>
2021-07-26 23:41:53 +0900

    ⚡️

--- src/pages/type.jsx
+++ src/pages/type.jsx
@@ -7,12 +7,6 @@ const Type = () => {
     <title>好走できる種牡馬一覧</title>
   </Head>
     <div>代表種牡馬一覧</div>
-    <h1>
-    <Link href="../grass">芝</Link>
-    </h1>
-    <h1>
-    <Link href="../sand">ダート</Link>
-    </h1>
     </>;
 };
 
