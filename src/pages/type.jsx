d00761702badea88cbe9405e4927885a0566acd1
jungle379 <jungle379@gmail.com>
2021-07-28 06:54:11 +0900

    ⚡️

--- src/pages/type.jsx
+++ src/pages/type.jsx
@@ -6,11 +6,12 @@ const Type = () => {
   <Head>
     <title>好走できる種牡馬一覧</title>
   </Head>
-    <div>代表種牡馬一覧(種付け費用順)</div>
+    <div>代表種牡馬一覧(2021年種付け費用順)</div>
     <div><Link href="/">ロードカナロア</Link></div>
     <div><Link href="/">キズナ</Link></div>
     <div><Link href="">エピファネイア</Link></div>
     <div><Link href="">ドゥラメンテ</Link></div>
+    <div><Link href="">モーリス</Link></div>
     </>;
 };
 
