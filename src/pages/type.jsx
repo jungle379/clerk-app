1db104b9ce7b4ad47957c09d945f1d20c55a73a4
jungle379 <jungle379@gmail.com>
2021-07-27 22:10:44 +0900

    🐛

--- src/pages/type.jsx
+++ src/pages/type.jsx
@@ -6,8 +6,14 @@ const Type = () => {
   <Head>
     <title>好走できる種牡馬一覧</title>
   </Head>
-    <div>代表種牡馬一覧</div>
+    <div>代表種牡馬一覧(種付け費用順)</div>
+    <div><Link href="/">ロードカナロア</Link></div>
+    <div><Link href="../race1">キズナ</Link></div>
+    <div><Link href="../race1">エピファネイア</Link></div>
+    <div><Link href="../race1">ドゥラメンテ</Link></div>
+    <div><Link href="../race1"></Link></div>
+    <div><Link href="../race1"></Link></div>
     </>;
 };
 
-export default Type;
+export default Type
