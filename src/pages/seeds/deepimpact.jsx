34be22aab39a5fd00e5c081dcfd9eb7028159b8a
jungle379 <jungle379@gmail.com>
2021-07-28 22:20:31 +0900

    🔥

---
+++ src/pages/seeds/deepimpact.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import Head from "next/head"
+
+const Seeds = () =>{
+  return <>
+  <Head>
+    <title>ディープインパクト</title>
+  </Head>
+    <div>ディープインパクト(ラストクロップ)</div>
+    <div>
+      <li>ハヤヤッコ</li>
+      <ul>スレイプニルS・2100m・OP・東京・田辺</ul>
+      <ul>大箱の競馬場が得意なのであろうか、４コーナーの終わりまで一番後ろで待機し、後方一期の末脚で差し切った。左の非根幹距離を狙う</ul>
+    <Link href="https://db.sp.netkeiba.com/horse/2016104624/">JRAページ</Link>
+    </div>
+    </>;
+};
+
+export default Seeds;
--- src/pages/type.jsx
+++ src/pages/type.jsx
@@ -7,6 +7,7 @@ const Type = () => {
     <title>好走できる種牡馬一覧</title>
   </Head>
     <div>代表種牡馬一覧(2021年種付け費用順)</div>
+    <div><Link href="/seeds/deepimpact">ディープインパクト(ラストクロップ・６頭)</Link></div>
     <div><Link href="/">ロードカナロア</Link></div>
     <div><Link href="/">キズナ</Link></div>
     <div><Link href="">エピファネイア</Link></div>
