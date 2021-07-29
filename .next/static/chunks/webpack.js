d8df92f05f89aa45da7db953a45d695dce3ff02b
jungle379 <jungle379@gmail.com>
2021-07-30 06:51:10 +0900

    ⚡️

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1be6d93a3a29c543aec4"; }
+/******/ 		__webpack_require__.h = function() { return "ca346723f5cc0cf7ae85"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/type.jsx
+++ src/pages/type.jsx
@@ -1,20 +1,18 @@
 import Link from "next/link";
 import Head from "next/head";
 
-const Type = (seeds) => {
+const Type = () => {
   return <>
   <Head>
     <title>好走できる種牡馬一覧</title>
   </Head>
-  <ul>
-  {seeds.map((seed) => (
-        <li key={seed.id}>
-          <Link href={`/seeds/${encodeURIComponent(seed.slug)}`}>
-            <a>{seed.title}</a>
-          </Link>
-        </li>
-      ))}
-      </ul>
+    <div>代表種牡馬一覧(2021年種付け費用順)</div>
+    <div><Link href="/seeds/deepimpact">ディープインパクト(ラストクロップ・６頭)</Link></div>
+    <div><Link href="/">ロードカナロア</Link></div>
+    <div><Link href="/">キズナ</Link></div>
+    <div><Link href="">エピファネイア</Link></div>
+    <div><Link href="">ドゥラメンテ</Link></div>
+    <div><Link href="">モーリス</Link></div>
     </>;
 };
 
