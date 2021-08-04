8b5345c1f65992109d9a6063587f9d980c34fd93
jungle379 <jungle379@gmail.com>
2021-08-04 22:47:17 +0900

    ⚡️ 動画を引用

--- src/pages/seeds/kizuna.jsx
+++ src/pages/seeds/kizuna.jsx
@@ -4,19 +4,22 @@ import Head from "next/head"
 const Seeds = () =>{
   return <>
   <Head>
-    <title>ディープインパクト</title>
+    <title>キズナ、母父ナスルーラ系</title>
   </Head>
-    <div>ディープインパクト(2021/ラストクロップ)</div>
+    <div>キズナ</div>
     <div>
       <li>代表産駒</li>
       <ul>3歳</ul>
-      <ul>・ランドオブリバティ：糸魚川特別(G3)</ul>
+      <ul>・：</ul>
       <ul>4歳</ul>
-      <ul>・コントレイル：ホープフルS、牡馬三冠</ul>
+      <ul>・ディープボンド：21年春天</ul>
     </div>
+    <div>芝：ローカルの牡馬は手を出さない。最内・大外は成績良し。左回りは弱い</div>
     <div>
-
+    ダート：３勝クラスまでで1800m以上
     </div>
+    <div>休み明けを狙うこと、牝馬の明け２戦目は買えない。脚質は追い込み以外、逃げ先行が逃げ残りする</div>
+    <Link href="https://www.youtube.com/watch?v=DKtGEJWgUnc">参考動画</Link>
     </>;
 };
 
