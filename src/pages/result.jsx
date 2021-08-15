552bf36f0d3350a9fc19f7b0e3a888c1ebd43a1c
jungle379 <jungle379@gmail.com>
2021-08-15 20:51:48 +0900

    🐛

--- src/pages/result.jsx
+++ src/pages/result.jsx
@@ -6,7 +6,7 @@ const Result = () =>{
   <Head>
     <title>今週の重賞</title>
   </Head>
-    <div>今週の重賞一覧</div>
+    <h1>今週の重賞一覧</h1>
     <h2>
     <Link href="../race1">スプリンターズS(G1)・芝1200m/10月3日/15:35</Link>
     </h2>
