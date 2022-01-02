e654f709357ea4445d56933dd92b2ad4cf7c4eb3
jungle379 <jungle379@gmail.com>
2022-01-02 23:39:18 +0900

    🔥

--- src/pages/memo.jsx
+++ src/pages/memo.jsx
@@ -1,19 +1,36 @@
 import Link from "next/link";
 import Head from "next/head";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Memo = () => {
-  return <>
-  <Head>
-    <title>種目別好走馬Index</title>
-  </Head>
-    <h1>勝利馬メモ</h1>
-    <h1>
-    <Link href="../grass">芝</Link>
-    </h1>
-    <h1>
-    <Link href="../sand">ダート</Link>
-    </h1>
-    </>;
+  return (
+    <>
+      <Head>
+        <title>種目別好走馬Index</title>
+      </Head>
+      <div className="bg-green-300 h-screen">
+        <Header />
+        <div>勝利馬メモ</div>
+        <h1>
+          <Link href="../grass">
+            <a>芝</a>
+          </Link>
+        </h1>
+        <h2>
+          <Link href="../sand">
+            <a>ダート</a>
+          </Link>
+        </h2>
+        <h3>
+          <Link href="/">
+            <a>障害レース</a>
+          </Link>
+        </h3>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Memo;
