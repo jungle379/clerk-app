d92dbd0fe03a55cffe9a1a3433e6cf5883daec1e
jungle379 <jungle379@gmail.com>
2022-01-04 17:07:39 +0900

    ⚡️ syuusei

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
 
@@ -23,13 +22,6 @@ const SignupLink = () => (
   <Link href="/sign-up">
     <a className="">
       <img src="/icons/user-plus.svg" />
-      <div>
-        <h3>Sign up for an account</h3>
-        <p>
-          Sign up and sign in to explore all the features provided by Clerk
-          out-of-the-box
-        </p>
-      </div>
       <div className="">
         <img src="/icons/arrow-right.svg" />
       </div>
@@ -43,7 +35,7 @@ const Header = () => (
       <div className="text-6xl font-bold text-pink-500 mx-40 my-10 py-[15px]">
         Welcome to Horse Racing Memo
       </div>
-      <div className="flex mr-20 my-20">
+      <div className="flex mr-10 my-20">
         <div className="mx-5">
           <SignedIn>
             <ClerkFeatures />
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <Head>
         <title>トップページ</title>
       </Head>
-      <div className="h-screen bg-green-300">
+      <div className="bg-green-300 h-full">
         <Header />
         <div className="text-4xl text-center font-bold">記録・分析など</div>
         <div className="flex justify-between text-2xl my-20 mx-20 px-40">
@@ -34,8 +34,26 @@ const Home = () => {
             <a>サイト作成者について</a>
           </Link>
         </div>
+        <div className="mx-20 my-20">
+          <h1 className="text-4xl font-bold">外部サイト</h1>
+          <h2 className="text-2xl font-bold my-10">
+            <Link href="https://www.netkeiba.com">
+              <a>netkeiba.com</a>
+            </Link>
+          </h2>
+          <h3 className="text-2xl font-bold my-10">
+            <Link href="/">
+              <a>血統ビーム</a>
+            </Link>
+          </h3>
+          <h4 className="my-10 text-2xl font-bold">
+            <Link href="">
+              <a>youtube(JRA公式)</a>
+            </Link>
+          </h4>
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </>
   );
 };
