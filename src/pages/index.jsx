539856497f0368d2ce0d133012be904327c5c38a
jungle379 <jungle379@gmail.com>
2022-01-19 22:20:51 +0900

    ⚡️

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -33,18 +33,18 @@ const Home = () => {
                 </Link>
               </div>
             </div>
-            <div className="flex justify-between text-2xl my-20">
-              <div className="text-2xl hover:text-red-400 font-bold px-5">
+            <div className="flex justify-between text-2xl my-20 px-5">
+              <div className="text-2xl hover:text-red-400 font-bold">
                 <Link href="../recommend">
                   <a>・一押しウマ</a>
                 </Link>
               </div>
-              <div className="text-2xl hover:text-red-400 font-bold px-5">
+              <div className="text-2xl hover:text-red-400 font-bold mx-5">
                 <Link href="../overseas">
                   <a>・海外開催</a>
                 </Link>
               </div>
-              <div className="text-2xl hover:text-red-400 font-bold px-5">
+              <div className="text-2xl hover:text-red-400 font-bold mx-14">
                 <Link href="../events">
                   <a>・新着情報</a>
                 </Link>
