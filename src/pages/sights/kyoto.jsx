4e658713288d9b9c5ba7da7f67e1f2335d6f1569
jungle379 <jungle379@gmail.com>
2022-01-22 12:42:23 +0900

    ⚡️

--- src/pages/sights/kyoto.jsx
+++ src/pages/sights/kyoto.jsx
@@ -2,30 +2,18 @@ import Head from "next/head";
 import Link from "next/link";
 
 const Tokyo = () => {
-
-return <>
-<Head>
-<title>京都競馬場について</title>
-</Head>
-<div>
-<h1>京都競馬場</h1>
-<h1>※令和5年春まで改修中のため、後ほど編集予定</h1>
-<h2>**</h2>
-</div>
-<h2>*</h2>
-<li>***</li>
-<ul>**</ul>
-<ul>**</ul>
-<li>***</li>
-
-<h2>*</h2>
-<li>***</li>
-<ul>**</ul>
-<ul>**</ul>
-<li>***</li>
-
-<Link href="/">JRA公式サイトへ</Link>
-</>;
-}
+  return (
+    <>
+      <Head>
+        <title>京都競馬場について</title>
+      </Head>
+      <div>
+        <h1>京都競馬場</h1>
+        <h2>※令和5年春まで改修中のため、後ほど編集予定</h2>
+      </div>
+      <Link href="/">JRA公式サイトへ</Link>
+    </>
+  );
+};
 
 export default Tokyo;
