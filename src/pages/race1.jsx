4cdc533e3e65535c1e2fa22185c7422ca9c1aa02
jungle379 <jungle379@gmail.com>
2022-01-23 18:51:37 +0900

    🔥 更新

--- src/pages/race1.jsx
+++ src/pages/race1.jsx
@@ -4,14 +4,14 @@ const Race1 = () => {
   return (
     <>
       <Head>
-        <title>東海S/中京/1800m</title>
+        <title>根岸S/東京/1400m</title>
       </Head>
-      <h1>東海S/中京/1800m</h1>
+      <h1>根岸S/東京/1400m</h1>
       <h2>馬場傾向</h2>
-      <li></li>
+      <li>開幕週の東京の砂で行われるが、差し馬に利点がある。</li>
       <h1>有力馬一覧</h1>
-      <div>デュードヴァン・ヒモ</div>
-      <div>アイオライト・ヒモ</div>
+      <div></div>
+      <div></div>
     </>
   );
 };
--- src/pages/race2.jsx
+++ src/pages/race2.jsx
@@ -4,14 +4,14 @@ const Race2 = () => {
   return (
     <>
       <Head>
-        <title>AJCC/中山/2200m</title>
+        <title>シルクロードS/中京/1200m</title>
       </Head>
-      <div>AJCC/中山/2200m</div>
+      <div>シルクロードS/中京/1200m</div>
       <li>馬場状況</li>
       <li></li>
       <div>有力馬一覧</div>
-      <div>オーソクレース・NGH</div>
-      <div>ポタジェ・ヒモ</div>
+      <div></div>
+      <div></div>
     </>
   );
 };
