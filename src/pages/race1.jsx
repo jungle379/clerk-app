91361d2ad27338a93881d4f77b8c478c5620db8f
jungle379 <jungle379@gmail.com>
2022-01-24 20:57:10 +0900

    🔥

--- src/pages/race1.jsx
+++ src/pages/race1.jsx
@@ -8,7 +8,7 @@ const Race1 = () => {
         <title>根岸S/東京/1400m</title>
       </Head>
       <div className="bg-green-300 h-screen">
-        <div className="text-4xl">根岸S/東京/1400m</div>
+        <div className="text-4xl">根岸S/東京ダート/1400m</div>
         <div className="text-2xl">馬場傾向</div>
         <li>開幕週の東京の砂で行われるが、差し馬に利点がある。</li>
         <div className="text-2xl">有力馬一覧</div>
--- src/pages/race2.jsx
+++ src/pages/race2.jsx
@@ -9,8 +9,12 @@ const Race2 = () => {
       </Head>
       <div className="bg-green-300 h-screen">
         <div className="text-4xl">シルクロードS/中京/1200m</div>
+        <li>馬場傾向</li>
+        <h1>
+          開幕が進んだ中京芝にて実施。3角〜4角に緩やかな降り。直線では最後に坂がある
+        </h1>
         <li>馬場状況</li>
-        <li></li>
+        <h2>近走を見て判断</h2>
         <div className="text-2xl">有力馬一覧</div>
         <div>メイケイエール</div>
         <div></div>
