2de0e744305afc5a8558cf59d695ce5fe40c3464
jungle379 <jungle379@gmail.com>
2022-01-28 22:24:14 +0900

    🔥

--- src/components/sidevar.jsx
+++ src/components/sidevar.jsx
@@ -4,32 +4,32 @@ const Sidevar = () => (
   <>
     <div className="mx-20 w-1/5 pr-20 h-full border-r-4">
       <h1 className="text-2xl font-bold">外部サイト</h1>
-      <h2 className="font-bold my-10 text-blue-500 hover:underline">
+      <h2 className="font-bold my-10 text-blue-700 hover:underline">
         <Link href="https://www.netkeiba.com">
           <a>netkeiba.com</a>
         </Link>
       </h2>
-      <h3 className="font-bold my-10 text-blue-500 hover:underline">
+      <h3 className="font-bold my-10 text-blue-700 hover:underline">
         <Link href="https://k-beam.com/">
           <a>血統ビームオフィシャルサイト</a>
         </Link>
       </h3>
-      <h4 className="my-10 font-bold text-blue-500 hover:underline">
+      <h4 className="my-10 font-bold text-blue-700 hover:underline">
         <Link href="https://www.youtube.com/user/jraofficial">
           <a>youtube(JRA公式)</a>
         </Link>
       </h4>
-      <h5 className="my-10 font-bold text-blue-500 hover:underline">
+      <h5 className="my-10 font-bold text-blue-700 hover:underline">
         <Link href="https://uma-jin.net/new/salon/salon_top">
           <a>UMAJIN 競馬サロン</a>
         </Link>
       </h5>
-      <h5 className="my-10 font-bold text-blue-500 hover:underline">
+      <h5 className="my-10 font-bold text-blue-700 hover:underline">
         <Link href="http://blog.livedoor.jp/ganma1123/">
           <a>邪推師GANMA 公式ブログ</a>
         </Link>
       </h5>
-      <div className="my-10 font-bold text-blue-500 hover:underline">
+      <div className="my-10 font-bold text-blue-700 hover:underline">
         <Link href="/auther">
           <a>作成者について</a>
         </Link>
