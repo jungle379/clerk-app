57b09003367be0c3921c4bcedbd111524e780f56
jungle379 <jungle379@gmail.com>
2022-01-30 20:24:53 +0900

    🔥

--- src/pages/race1.jsx
+++ src/pages/race1.jsx
@@ -5,17 +5,15 @@ const Race1 = () => {
   return (
     <>
       <Head>
-        <title>根岸S/東京/1400m</title>
+        <title>東京新聞杯/東京/1600m</title>
       </Head>
       <div className="bg-green-300 h-screen">
-        <div className="text-4xl">根岸S/東京ダート/1400m</div>
+        <div className="text-4xl">東京新聞杯/東京/1600m</div>
         <div className="text-2xl">馬場傾向</div>
-        <li>開幕週の東京の砂で行われるが、差し馬/追い込み馬に利点がある。</li>
-        <li>逃げ馬で残ったのはモーニンやコパノキッキングなど有力馬のみ</li>
         <div className="text-2xl">有力馬一覧</div>
-        <div>オメガレインボー・NGH/差し・追い込み馬</div>
-        <div>タガノビューティー・NGH/差し・追い込み馬</div>
-        <div>スリーグラント・紐解き</div>
+        <div></div>
+        <div></div>
+        <div></div>
         <Footer />
       </div>
     </>
--- src/pages/race2.jsx
+++ src/pages/race2.jsx
@@ -5,18 +5,16 @@ const Race2 = () => {
   return (
     <>
       <Head>
-        <title>シルクロードS/中京/1200m</title>
+        <title>きさらぎ賞/中京/2000m</title>
       </Head>
       <div className="bg-green-300 h-screen">
-        <div className="text-4xl">シルクロードS/中京/1200m</div>
+        <div className="text-4xl">きさらぎ賞/中京/2000m</div>
         <li>馬場傾向</li>
-        <h1>
-          開幕が進んだ中京芝にて実施。3角〜4角に緩やかな降り。直線では最後に坂がある
-        </h1>
+        <li></li>
         <li>馬場状況</li>
-        <h2>近走を見て判断</h2>
         <div className="text-2xl">有力馬一覧</div>
-        <div>メイケイエール/先行？</div>
+        <div>リューベック</div>
+        <div></div>
         <div></div>
         <Footer />
       </div>
