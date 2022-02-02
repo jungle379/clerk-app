62e34b11f7a4363bfc5c3132ab754b3fb39d893a
jungle379 <jungle379@gmail.com>
2022-02-02 21:11:16 +0900

    ⚡️

--- src/pages/predict.jsx
+++ src/pages/predict.jsx
@@ -15,16 +15,16 @@ const Predict = () => {
         <div className="flex py-10">
           <Sidevar />
           <div className="md:text-4xl mx-10 text-pink-400 font-bold w-4/5">
-            今週の重賞一覧
+            今週の重賞
             <div className="md:text-2xl my-10 text-blue-600">
               <h1 className="border-2 h-[120px] py-10 px-5 hover:underline">
                 <Link href="../race1">
-                  <a>東京新聞杯(G3)15:45</a>
+                  <a>東京新聞杯(G3)15:45/1600m</a>
                 </Link>
               </h1>
               <h2 className="border-2 h-[120px] py-10 px-5 hover:underline">
                 <Link href="../race2">
-                  <a>きさらぎ賞(G3)15:35</a>
+                  <a>きさらぎ賞(G3)15:35/2000m</a>
                 </Link>
               </h2>
             </div>
