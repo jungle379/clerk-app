bb5d9068c12b5c91e97d2377c37be01cb8687cac
jungle379 <jungle379@gmail.com>
2022-02-02 21:13:23 +0900

    🔥

--- src/pages/race1.jsx
+++ src/pages/race1.jsx
@@ -9,11 +9,11 @@ const Race1 = () => {
       </Head>
       <div className="bg-green-300 h-screen">
         <div className="text-4xl">東京新聞杯/東京/1600m</div>
-        <div className="text-2xl">馬場傾向</div>
+        <div className="text-2xl my-10">馬場傾向</div>
         <div>
           後傾ラップになりがちだがある程度の位置にいないと差しが届かない。3角から下りのため息を入れる余地がない。
         </div>
-        <div className="text-2xl">有力馬一覧</div>
+        <div className="text-2xl my-10">有力馬一覧</div>
         <div>カラテ</div>
         <div>ファインルージュ・NGH</div>
         <div></div>
--- src/pages/race2.jsx
+++ src/pages/race2.jsx
@@ -8,11 +8,11 @@ const Race2 = () => {
         <title>きさらぎ賞/中京/2000m</title>
       </Head>
       <div className="bg-green-300 h-screen">
-        <div className="text-4xl">きさらぎ賞/中京/2000m</div>
+        <div className="text-4xl mb-10">きさらぎ賞/中京/2000m</div>
         <li>馬場傾向</li>
         <li>ディープ系など主要産駒が好走。昨年は3番人気までで決着。</li>
         <li>馬場状況</li>
-        <div className="text-2xl">有力馬一覧</div>
+        <div className="text-2xl my-10">有力馬一覧</div>
         <div>リューベック</div>
         <div></div>
         <div></div>
