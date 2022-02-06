1a8921f46b41ecff22a5a8974e027d565be0f371
jungle379 <jungle379@gmail.com>
2022-02-06 18:25:44 +0900

    🎨

--- src/pages/race1.jsx
+++ src/pages/race1.jsx
@@ -5,20 +5,13 @@ const Race1 = () => {
   return (
     <>
       <Head>
-        <title>東京新聞杯/東京/1600m</title>
+        <title>****賞/**/**m</title>
       </Head>
       <div className="bg-green-300 h-screen">
-        <div className="text-4xl">東京新聞杯/東京/1600m</div>
-        <div className="text-2xl my-10">馬場傾向</div>
-        <div>
-          後傾ラップになりがちだがある程度の位置にいないと差しが届かない。3角から下りのため息を入れる余地がない。
-        </div>
+        <div className="text-4xl mb-10">Race1</div>
+        <li>馬場傾向</li>
+        <li>馬場状況</li>
         <div className="text-2xl my-10">有力馬一覧</div>
-        <div>カラテ</div>
-        <div>ファインルージュ・NGH・ヒモ</div>
-        <div>エイシンチラー・ヒモ</div>
-        <div>ホウオウアマゾン</div>
-        <div></div>
         <Footer />
       </div>
     </>
--- src/pages/race2.jsx
+++ src/pages/race2.jsx
@@ -5,17 +5,13 @@ const Race2 = () => {
   return (
     <>
       <Head>
-        <title>きさらぎ賞/中京/2000m</title>
+        <title>****賞/**/**m</title>
       </Head>
       <div className="bg-green-300 h-screen">
-        <div className="text-4xl mb-10">きさらぎ賞/中京/2000m</div>
+        <div className="text-4xl mb-10">Race2</div>
         <li>馬場傾向</li>
-        <li>ディープ系など主要産駒が好走。昨年は3番人気までで決着。</li>
         <li>馬場状況</li>
         <div className="text-2xl my-10">有力馬一覧</div>
-        <div>フォースクエア・ヒモ</div>
-        <div>セルケト</div>
-        <div>ダンテスビュー</div>
         <Footer />
       </div>
     </>
