a8ae7a58644eecb2a3ab40f7b44932cfc4312840
jungle379 <jungle379@gmail.com>
2022-02-08 21:40:40 +0900

    🐛

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,11 @@ const Footer = () => (
   <>
     <div className="bg-green-300 border-t-2">
       <div className="flex justify-between mt-10 mx-10 px-20">
-        <div className="hover:text-red-500">このサイトについて</div>
+        <div className="hover:text-red-500">
+          <Link href="">
+            <a>このサイトについて</a>
+          </Link>
+        </div>
         <div className="hover:text-red-500">***</div>
         <div className="hover:text-red-500">***</div>
         <div className="hover:text-red-500">***</div>
