a3644d31e955b2363db5bd69852d3ae661c4fe31
jungle379 <jungle379@gmail.com>
2022-02-11 13:57:17 +0900

    ⚡️ style-changed

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -22,6 +22,11 @@ const Home = () => {
                   <a>・今週の予想</a>
                 </Link>
               </div>
+              <div className="text-2xl hover:text-red-400 font-bold">
+                <Link href="/">
+                  <a>・先週の結果</a>
+                </Link>
+              </div>
               <div className="hover:text-red-400">
                 <Link href="../type">
                   <a>・血統、産駒</a>
@@ -47,18 +52,6 @@ const Home = () => {
               <div className="text-2xl hover:text-red-400 font-bold mx-14">
                 <Link href="../news/latest">
                   <a>・新着情報</a>
-                </Link>
-              </div>
-            </div>
-            <div className="flex justify-evenly text-2xl my-20 px-5">
-              <div className="text-2xl hover:text-red-400 font-bold">
-                <Link href="/">
-                  <a>・先週の結果</a>
-                </Link>
-              </div>
-              <div className="text-2xl hover:text-red-400 font-bold">
-                <Link href="/">
-                  <a>・その他</a>
                 </Link>
               </div>
             </div>
