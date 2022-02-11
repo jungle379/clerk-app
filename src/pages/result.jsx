17614ecdffd656d1e1109378d4a6f3d8d85bd105
jungle379 <jungle379@gmail.com>
2022-02-11 14:21:13 +0900

    ⚡️

--- src/pages/result.jsx
+++ src/pages/result.jsx
@@ -4,18 +4,18 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Sidevar from "../components/sidevar";
 
-const Predict = () => {
+const Result = () => {
   return (
     <>
       <Head>
-        <title>今週の重賞予想</title>
+        <title>先週の結果</title>
       </Head>
       <div className="h-auto bg-green-300">
         <Header />
         <div className="flex py-10">
           <Sidevar />
           <div className="md:text-4xl mx-10 text-pink-400 font-bold w-4/5">
-            今週の重賞
+            先週の結果
             <div className="md:text-2xl my-10 text-blue-600">
               <h1 className="border-2 h-[120px] py-10 px-5 hover:underline">
                 <Link href="../race1">
@@ -36,4 +36,4 @@ const Predict = () => {
   );
 };
 
-export default Predict;
+export default Result;
