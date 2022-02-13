d5bff7f4c4887594c036dd5809c2c5b7f8b18064
jungle379 <jungle379@gmail.com>
2022-02-13 22:11:01 +0900

    ⚡️

--- src/pages/predict.jsx
+++ src/pages/predict.jsx
@@ -16,18 +16,18 @@ const Predict = () => {
           <Sidevar />
           <div className="md:text-4xl mx-10 text-pink-400 font-bold w-4/5">
             今週の重賞
-            {/* <div className="md:text-2xl my-10 text-blue-600">
+            <div className="md:text-2xl my-10 text-blue-600">
               <h1 className="border-2 h-[120px] py-10 px-5 hover:underline">
                 <Link href="../race1">
-                  <a>東京新聞杯(G3)15:45/1600m</a>
+                  <a>フェブラリーS(G1)15:45/1600m</a>
                 </Link>
               </h1>
-              <h2 className="border-2 h-[120px] py-10 px-5 hover:underline">
+              {/* <h2 className="border-2 h-[120px] py-10 px-5 hover:underline">
                 <Link href="../race2">
                   <a>きさらぎ賞(G3)15:35/2000m</a>
                 </Link>
-              </h2>
-            </div> */}
+              </h2> */}
+            </div>
           </div>
         </div>
         <Footer />
--- src/pages/race1.jsx
+++ src/pages/race1.jsx
@@ -5,13 +5,16 @@ const Race1 = () => {
   return (
     <>
       <Head>
-        <title>****賞/**/**m</title>
+        <title>フェブラリーS/東京/砂・1600m</title>
       </Head>
       <div className="bg-green-300 h-screen">
-        <div className="text-4xl mb-10">Race1</div>
+        <div className="text-4xl mb-10">フェブラリーS</div>
         <li>馬場傾向</li>
         <li>馬場状況</li>
         <div className="text-2xl my-10">有力馬一覧</div>
+        <div>ソリストサンダー・NGH</div>
+        <div></div>
+        <div></div>
         <Footer />
       </div>
     </>
