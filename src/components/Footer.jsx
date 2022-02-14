6de1e764b59f0da53302062e86d4921afcfa716b
jungle379 <jungle379@gmail.com>
2022-02-14 20:43:25 +0900

    🐛

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import Link from "next/Link";
 const Footer = () => (
   <>
     <div className="bg-green-300n border-t-2">
-      <div className="flex justify-between mt-10 mx-10 px-20">
+      <div className="flex justify-between mt-5 mx-10 px-20">
         <div className="hover:text-red-500">
           <Link href="/">
             <a>このサイトについて</a>
--- src/pages/race1.jsx
+++ src/pages/race1.jsx
@@ -8,15 +8,17 @@ const Race1 = () => {
         <title>フェブラリーS/東京/砂・1600m</title>
       </Head>
       <div className="bg-green-300 h-screen">
-        <div className="text-4xl mb-10">フェブラリーS</div>
-        <li>馬場傾向</li>
-        <ul>逃げ先行が有利ながら差しにも展開が向く。</ul>
-        <li>馬場状況</li>
-        <ul>土日に雨が想定、従来よりスピード重視される。</ul>
-        <div className="text-2xl my-10">有力馬一覧</div>
-        <div>ソリストサンダー・NGH</div>
-        <div>レッドルゼル・絶対軸</div>
-        <div></div>
+        <div className="text-6xl mx-10 py-10">フェブラリーS</div>
+        <div className="mx-20 text-2xl my-10">
+          <li>馬場傾向</li>
+          <ul>逃げ先行が有利ながら差しにも展開が向く。</ul>
+          <li>馬場状況</li>
+          <ul>土日に雨が想定、従来よりスピード重視される。</ul>
+          <div className="text-4xl my-10">有力馬一覧</div>
+          <div>ソリストサンダー・NGH</div>
+          <div>レッドルゼル・絶対軸・最悪データ</div>
+          <div></div>
+        </div>
         <Footer />
       </div>
     </>
