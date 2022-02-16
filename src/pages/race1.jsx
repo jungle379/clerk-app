289d4fe42d80d42d0610854f047de54f185ad21d
jungle379 <jungle379@gmail.com>
2022-02-16 21:48:22 +0900

    🐛

--- src/pages/race1.jsx
+++ src/pages/race1.jsx
@@ -12,11 +12,12 @@ const Race1 = () => {
         <div className="mx-20 text-2xl my-10">
           <li>馬場傾向</li>
           <ul>逃げ先行が有利ながら差しにも展開が向く。</ul>
+          <ul>上がり勝負になり、11番手以下で4角を迎えると最速の足が必要</ul>
           <li>馬場状況</li>
           <ul>土日に雨が想定、従来よりスピード重視される。</ul>
           <div className="text-4xl my-10">有力馬一覧</div>
-          <div>ソリストサンダー・NGH</div>
-          <div>レッドルゼル・絶対軸・最悪データ</div>
+          <div>・ソリストサンダー・NGH</div>
+          <div>・レッドルゼル・絶対軸・最悪データ</div>
           <div></div>
         </div>
         <Footer />
