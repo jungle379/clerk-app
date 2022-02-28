58c65c9eaa8509af474fd6859d52e6c452ba1577
jungle379 <jungle379@gmail.com>
2022-02-28 21:47:36 +0900

    🐛

--- src/pages/race1.jsx
+++ src/pages/race1.jsx
@@ -8,12 +8,12 @@ const Race1 = () => {
         <title>弥生賞/中山/2000m</title>
       </Head>
       <div className="bg-green-300 h-screen">
-        <div className="text-6xl mx-10 py-10">弥生賞</div>
+        <div className="text-4xl mx-10 py-10">弥生賞</div>
         <div className="mx-20 text-2xl my-10">
           <li>馬場傾向</li>
           <ul className="px-10"></ul>
           <li>馬場状況</li>
-          <div className="text-4xl my-10">有力馬一覧</div>
+          <div className="text-2xl my-10">有力馬一覧</div>
           <div>・</div>
           <div></div>
           <div></div>
