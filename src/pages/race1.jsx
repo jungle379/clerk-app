a5538f353ffe01fac98441165609fa978d801e33
jungle379 <jungle379@gmail.com>
2022-03-12 21:03:40 +0900

    ⚡️

--- src/pages/race1.jsx
+++ src/pages/race1.jsx
@@ -8,15 +8,13 @@ const Race1 = () => {
         <title>中山牝馬S/中山/1800m</title>
       </Head>
       <div className="bg-green-300 h-screen">
-        <div className="text-4xl mx-10 py-10">中山牝馬S</div>
+        <div className="text-4xl mx-10 py-10"></div>
         <div className="mx-20 text-2xl my-10">
           <li>馬場傾向</li>
           <ul className="px-10"></ul>
           <li>馬場状況</li>
           <ul></ul>
           <div className="text-2xl my-10">有力馬一覧</div>
-          <div>・ドナアトラエンテ/絶対軸</div>
-          <div>・クールキャット/紐解き</div>
         </div>
         <Footer />
       </div>
